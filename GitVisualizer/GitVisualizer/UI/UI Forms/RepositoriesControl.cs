0b7165db0e78a71419d6d765209b2e86d7863623
Nam Do <namdo1204@gmail.com>
2023-11-09 23:13:22 -0700

    Created unit testing and namespace for Github API Implemented: - Starter code for unit testing that is in a sub-project. - Created a namespace for Github.cs and insert "using" for any files needing Github.cs

--- GitVisualizer/GitVisualizer/UI/UI Forms/RepositoriesControl.cs
+++ GitVisualizer/GitVisualizer/UI/UI Forms/RepositoriesControl.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using GithubSpace;
+
 namespace GitVisualizer.UI.UI_Forms
 {
     public partial class RepositoriesControl : UserControl
